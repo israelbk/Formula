f14eea2a407de5e130a87b18ee7e83cbc87a7c99
Israelbk <israel727@gmail.com>
2021-04-27 01:52:03 +0300

    Layout refactor

--- src/app/data-provider/data-distrebuter.service.ts
+++ src/app/data-provider/data-distrebuter.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
-import { merge, Observable, of } from 'rxjs';
+import { merge, Observable, timer } from 'rxjs';
 import {
-  delay,
   distinctUntilChanged,
   filter,
   map,
+  mapTo,
   switchMap,
 } from 'rxjs/operators';
 import { DataProviderService } from './data-provider.service';
@@ -13,42 +13,44 @@ import { DataProviderService } from './data-provider.service';
   providedIn: 'root',
 })
 export class DataDistrebuterService {
-  readonly cerror$ = this.initBoolObservable('cerror');
-  readonly pedal$ = this.initNumberOservable('pedal');
-  readonly rpm$ = this.initNumberOservable('rpm');
-  readonly speed$ = this.initNumberOservable('speed');
-  readonly thorttle$ = this.initNumberOservable('thorttle');
-  readonly voltage$ = this.initNumberOservable('voltage');
-  readonly brake$ = this.initBoolObservable('brake');
-  readonly perror$ = this.initBoolObservable('perror');
-  readonly bspd$ = this.initBoolObservable('bspd');
-  readonly terror$ = this.initBoolObservable('terror');
+  private readonly errorObservable$ = this.dataProviderService.recievedData$.pipe(
+    switchMap(() => timer(200)),
+    mapTo(null)
+  );
+
+  // Boolean observables.
+  readonly cerror$ = this.getBoolStream('cerror');
+  readonly brake$ = this.getBoolStream('brake');
+  readonly perror$ = this.getBoolStream('perror');
+  readonly bspd$ = this.getBoolStream('bspd');
+  readonly terror$ = this.getBoolStream('terror');
+
+  // Number observables.
+  readonly pedal$ = this.getNumberStream('pedal');
+  readonly rpm$ = this.getNumberStream('rpm');
+  readonly speed$ = this.getNumberStream('speed');
+  readonly thorttle$ = this.getNumberStream('thorttle');
+  readonly voltage$ = this.getNumberStream('voltage');
 
   constructor(private dataProviderService: DataProviderService) {}
 
-  private initBoolObservable(dataKey: string): Observable<boolean> {
+  private getBoolStream(dataKey: string): Observable<boolean | null> {
     const data$ = this.dataProviderService.recievedData$.pipe(
       filter((recievedData) => recievedData.key === dataKey),
       map((record) => Boolean(record.value)),
       distinctUntilChanged()
     );
 
-    return merge(data$, this.errorObservable());
+    return merge(data$, this.errorObservable$);
   }
 
-  private initNumberOservable(dataKey: string): Observable<number> {
+  private getNumberStream(dataKey: string): Observable<number> {
     const data$ = this.dataProviderService.recievedData$.pipe(
       filter((recievedData) => recievedData.key === dataKey),
       map((data) => +data.value),
       distinctUntilChanged()
     );
 
-    return merge(data$, this.errorObservable());
-  }
-
-  private errorObservable(): Observable<any> {
-    return this.dataProviderService.recievedData$.pipe(
-      switchMap(() => of(undefined).pipe(delay(200)))
-    );
+    return merge(data$, this.errorObservable$);
   }
 }
