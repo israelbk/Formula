df362c646d847566274247e15f3b889b9ff244a9
Israelbk <israel727@gmail.com>
2021-05-05 01:59:17 +0300

    Adding the rest of the numeric indicators

--- src/app/indications/indicators/numeric-indicator/numeric-indicator.component.ts
+++ src/app/indications/indicators/numeric-indicator/numeric-indicator.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { DataDistrebuterService } from 'src/app/data-provider/data-distrebuter.service';
 
 @Component({
   selector: 'app-numeric-indicator',
   templateUrl: './numeric-indicator.component.html',
-  styleUrls: ['./numeric-indicator.component.css']
+  styleUrls: ['./numeric-indicator.component.css'],
 })
 export class NumericIndicatorComponent implements OnInit {
+  constructor(readonly dataDistrebuterService: DataDistrebuterService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
