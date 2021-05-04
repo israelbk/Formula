2781e2c660459d122e5e2941b9815143cac71e84
Israelbk <israel727@gmail.com>
2021-05-05 00:08:05 +0300

    Adding structure for seven segment display
    # Conflicts:
    #	src/app/app.module.ts
    #	src/app/indications/indicators/indicators.component.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { NumberDisplayComponent } from './indications/seven-segment/number-display/number-display.component';
 import { BoolToColorPipe } from './indications/colors/pipes/bool-to-color.pipe';
 import { IndicatorsComponent } from './indications/indicators/indicators.component';
 import { AnimatedDrivingCarComponent } from './footer/animated-driving-car/animated-driving-car.component';
@@ -8,6 +9,7 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { ColorToClassPipe } from './indications/colors/pipes/color-to-class.pipe';
 import { NumericIndicatorComponent } from './indications/indicators/numeric-indicator/numeric-indicator.component';
+import { BasicNumberIndicatorComponent } from './indications/seven-segment/digit-display/digit-display.component';
 
 @NgModule({
   declarations: [
@@ -16,6 +18,8 @@ import { NumericIndicatorComponent } from './indications/indicators/numeric-indi
     BoolCarIndicatorComponent,
     IndicatorsComponent,
     AnimatedDrivingCarComponent,
+    BasicNumberIndicatorComponent,
+    NumberDisplayComponent,
     ColorToClassPipe,
     BoolToColorPipe,
     NumericIndicatorComponent,
---
+++ src/app/indications/seven-segment/number-display/number-display.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { NumberDisplayComponent } from './number-display.component';
+
+describe('NumberDisplayComponent', () => {
+  let component: NumberDisplayComponent;
+  let fixture: ComponentFixture<NumberDisplayComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ NumberDisplayComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NumberDisplayComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
